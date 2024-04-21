c3c12545b96f12b5d51b770b969ea0c25b333408
terrence-ou <outerrencedl@gmail.com>
2024-04-21 19:44:19 -0400

    config file updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   site: "https://terrence-ou.github.io",
+  base: "/",
   integrations: [
     mdx(),
     sitemap(),
