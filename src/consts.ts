c1ed24ea1e25a4d43694a2d89dbd38925e8f5ffc
terrence-ou <outerrencedl@gmail.com>
2024-04-22 08:55:45 -0400

    footer updated

--- src/consts.ts
+++ src/consts.ts
@@ -54,28 +54,28 @@ export const LINKS: Links = [
 
 // Socials
 export const SOCIALS: Socials = [
-  {
-    NAME: "Email",
-    ICON: "email",
-    TEXT: "markhorn.dev@gmail.com",
-    HREF: "mailto:markhorn.dev@gmail.com",
-  },
+  // {
+  //   NAME: "Email",
+  //   ICON: "email",
+  //   TEXT: "markhorn.dev@gmail.com",
+  //   HREF: "mailto:markhorn.dev@gmail.com",
+  // },
   {
     NAME: "Github",
     ICON: "github",
-    TEXT: "markhorn-dev",
-    HREF: "https://github.com/markhorn-dev/astro-sphere",
+    TEXT: "https://github.com/terrence-ou",
+    HREF: "https://github.com/terrence-ou",
   },
-  {
-    NAME: "LinkedIn",
-    ICON: "linkedin",
-    TEXT: "markhorn-dev",
-    HREF: "https://www.linkedin.com/in/markhorn-dev/",
-  },
+  // {
+  //   NAME: "LinkedIn",
+  //   ICON: "linkedin",
+  //   TEXT: "markhorn-dev",
+  //   HREF: "https://www.linkedin.com/in/markhorn-dev/",
+  // },
   {
     NAME: "Twitter",
     ICON: "twitter-x",
-    TEXT: "markhorn_dev",
-    HREF: "https://twitter.com/markhorn_dev",
+    TEXT: "https://twitter.com/ou_terrence",
+    HREF: "https://twitter.com/ou_terrence",
   },
 ];
